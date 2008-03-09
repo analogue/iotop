a19ed6e4874974637e714ba46571c8153cb3b36f
Guillaume Chazarain <guichaz@yahoo.fr>
2008-03-09 22:58:41 +0100

    Added packaging information

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+
+sys.path.insert(0, 'setuptools-0.6c6-py2.4.egg')
+from setuptools import setup, find_packages
+from iotop.version import VERSION
+
+setup(name='iotop',
+      version=VERSION,
+      description='Per process I/O bandwidth monitor',
+      long_description=
+'''Iotop is a Python program with a top like UI used to show of behalf of which
+process is the I/O going on.''',
+      author='Guillaume Chazarain',
+      author_email='guichaz@gmail.com',
+      url='http://guichaz.free.fr/iotop',
+      scripts=['bin/iotop'],
+      packages=find_packages(),
+      include_package_data=True,
+      license='GPL'
+)
