analogue/iotop

commit d4cab23b1c8c2f91ae7b353087bc60e7659620ef

Some more minor optimizations

iotop/data.py
@@ -82,16 +82,18 @@ class Stats(DumpableObject):
         return self.accumulate(other_stats, destination, coeff=-1)
 
     def is_all_zero(self):
+        sd = self.__dict__
         for name, offset in Stats.members_offsets:
-            if getattr(self, name) != 0:
+            if sd[name] is not 0:
                 return False
         return True
 
     @staticmethod
     def build_all_zero():
         stats = Stats.__new__(Stats)
+        std = stats.__dict__
         for name, offset in Stats.members_offsets:
-            setattr(stats, name, 0)
+            std[name] = 0
         return stats
 
 #

commit e0277ae60b0520fab315f654e48745cfda5df37f

commit d4cab23b1c8c2f91ae7b353087bc60e7659620ef broke iotop -o

iotop/data.py
@@ -84,7 +84,7 @@ class Stats(DumpableObject):
     def is_all_zero(self):
         sd = self.__dict__
         for name, offset in Stats.members_offsets:
-            if sd[name] is not 0:
+            if sd[name] != 0:
                 return False
         return True
 