488c381c5301824c3755f5e5460adffaac928653
Guillaume Chazarain <guichaz@gmail.com>
2013-02-03 18:34:30 +0100

    The RPM should also install to sbin.

--- .install-rpm.sh
+++ .install-rpm.sh
@@ -8,3 +8,9 @@ sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.gz@g' INS
 # actually, it doesn't on all distributions so just compress unconditionally
 # before brp-compress is run
 find "$RPM_BUILD_ROOT" -type f -name iotop.8 -exec gzip '{}' \;
+
+# move from bin/ to sbin/
+sed -i -e 's@/bin/iotop@/sbin/iotop@g' INSTALLED_FILES
+cd "$(find "$RPM_BUILD_ROOT" -type d -name bin)/.."
+mv bin sbin
+cd -
